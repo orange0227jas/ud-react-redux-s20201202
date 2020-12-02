fb118d7619672810befb53e0a010708c0e787534
orange0227jas <orange.1224.go@gmail.com>
2020-12-03 07:20:27 +0900

    implement component _connect function

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,28 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux'
 
-// class App extends Component {
-//   render (){
-//     return( 
-//     <>
-//     <div>
-//       <label htmlFor="bar">bar</label>
-//       <input type="text" onChange={()=> {console.log("I am moti")}}/>;
-//     </div>
-//     </>
-//     )
-//   }
-// }
+import { increment, decrement } from '../actions'
 
-const App = () => {
-  return (
-    <>
-    <Counter />
-    </>
-  )
-}
-
-class Counter extends Component {
-  constructor(props){
-    super(props)
-    this.state ={count:0 }
-  }
-
-  handlePlusButton = ()=> {
-    this.setState({ count: this.state.count + 1 } )
-    //状態を帰るときには、setStateを使うこと。setState実行時、再renderされる。
-  }
-  handleMinusButton = () => {
-    this.setState({ count: this.state.count - 1 })
-  }
+class App extends Component {
   render () {
+    const props = this.props
+
     return (
     <>
-      <div>counter: { this.state.count }</div>
-      <button onClick={this.handlePlusButton} >+1</button>
-      <button onClick={this.handleMinusButton}>-1</button>
+      <div>value: { props.value }</div>
+      <button onClick={props.increment} >+1</button>
+      <button onClick={props.decrement} >-1</button>
     </>
     )
   }
 }
-  
-//isRequiredは空白だった場合に検知できる。
 
-export default App;
+const mapStateToProps = state => ({ value: state.count.value })
+// const mapDispatchToProps = dispatch => ({
+//   increment:() => dispatch(increment()),
+//   decrement:() => dispatch(decrement())
+// })
+//↑↓同じ意味になる。
+const mapDispatchToProps = ({ increment, decrement })
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
