28b1e1db40e6c02b9033cba9238c1356435ed576
orange0227jas <orange.1224.go@gmail.com>
2020-12-02 16:50:41 +0900

    create function Components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render (){
-    return( 
+// class App extends Component {
+//   render (){
+//     return( 
+//     <>
+//     <div>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onChange={()=> {console.log("I am moti")}}/>;
+//     </div>
+//     </>
+//     )
+//   }
+// }
+
+const App = () => {
+  return (
     <>
-    <div>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={()=> {console.log("I am moti")}}/>;
-    </div>
+    <Moti />
+    <Moti />
+    <Moti />
+    <Moti />
     </>
-    )
-  }
+  )
+}
+
+const Moti = () => {
+  return <div>moti!!!</div>
 }
 
 export default App;
