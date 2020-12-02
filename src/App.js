34958aa7cdfde186ced0871f35345ec2387a14b4
System Administrator <root@miyamotoshougonoMacBook-Air.local>
2020-12-02 13:56:19 +0900

    change welcome message to It's A react Practice

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          It's A React Practice!!
         </a>
       </header>
     </div>
