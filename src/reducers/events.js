a108a6c22bb6181c9a57e1cff26636cacaf1e6a7
orange0227jas <orange.1224.go@gmail.com>
2020-12-05 00:12:54 +0900

    event.js modify

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -3,14 +3,17 @@ import _ from 'lodash'
 import { 
     READ_EVENTS,
     READ_EVENT,
+    CREATE_EVENT,
+    UPDATE_EVENT,
     DELETE_EVENT,
 } from '../actions'
 
 export default ( events={}, action) =>{
     switch (action.type) {
+        case CREATE_EVENT:
         case READ_EVENT:
+        case UPDATE_EVENT:
             const data = action.response.data
-            // {id: 9, title: "Let's have an event 9!", body: "This is the body for event 9."}
             return { ...events, [data.id]: data } 
         case READ_EVENTS:
             return _.mapKeys(action.response.data, 'id')
